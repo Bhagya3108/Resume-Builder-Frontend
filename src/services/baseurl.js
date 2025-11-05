c12913dfd9144430b84e4a0f8df001ff5e6535cd
bhagya3108 <bhagyalakshmiku31@gmail.com>
2025-11-05 13:18:22 +0530

    editedurl

--- src/services/baseurl.js
+++ src/services/baseurl.js
@@ -1,5 +1,5 @@
 // const baseUrl='http://localhost:3000'
-const baseUrl='https://resume-buider-backend.onrender.com/resumes'
+const baseUrl='https://resume-buider-backend.onrender.com/resumesgit'
 
 // for connecting frontend and severside
 export default baseUrl
